cf53cf07506efd85273af8e528a60043bce4b175
shirdi <shirdivinayak@gmail.com>
2024-09-19 17:01:59 +0530

    fixed changed excel data to json

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,8 @@ interface Report {
   salary: number;
 }
 
-// Apply CORS middleware
-app.use('/api/*', cors());
+
+app.use('/api/*', cors())
 app.use(
   '/api2/*',
   cors({
@@ -24,8 +24,14 @@ app.use(
     maxAge: 600,
     credentials: true,
   })
-);
+)
 
+app.all('/api/abc', (c) => {
+  return c.json({ success: true })
+})
+app.all('/api2/abc', (c) => {
+  return c.json({ success: true })
+})
 // Load data from JSON file
 const readJSONData = (): Report[] => {
   const filePath = path.resolve(__dirname, '../data/salaries.json');
